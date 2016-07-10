296d6c7989969c63e3402be53e25d5d7e4d1c733
anyabelle <anutakeba@yahoo.com>
2016-07-10 12:13:03 +0300

    Horizontal Line added

---
+++ SnakeS/SnakeS/HorisontalLine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeS
+{
+    class HorisontalLine
+    {
+        List<Point> Line = new List<Point>();
+        public HorisontalLine(int xl, int xr, int y, char sym)
+        {
+            for (int i = xl; i <= xr; i++)
+            {
+                Line.Add(new Point(i, y, sym));
+            }
+        }
+        public void Draw()
+        {
+            foreach (Point p in Line)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
--- SnakeS/SnakeS/Program.cs
+++ SnakeS/SnakeS/Program.cs
@@ -10,17 +10,8 @@ namespace SnakeS
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(4, 3, '*');
-            p1.Draw();
-            List<Point> ll = new List<Point>();
-            ll.Add(p1);
-            ll.Add(p2);
-            foreach (Point pi in ll)
-            {
-                Console.WriteLine(pi.x);
-            }
-            Console.WriteLine("Hello");
+            HorisontalLine ll = new HorisontalLine(2, 5, 7, '+');
+            ll.Draw();
             Console.ReadLine();
         }
     }
